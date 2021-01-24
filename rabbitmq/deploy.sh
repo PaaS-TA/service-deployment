fc2bcb2e5dd90e9faa03f112b3d63dca03decdcd
okpc579 <ski579579@naver.com>
2021-01-24 13:21:28 +0900

    marketplace delete, deploy.sh modify

--- rabbitmq/deploy.sh
+++ rabbitmq/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # VARIABLES
-BOSH_NAME="micro-bosh"                           # bosh name (e.g. micro-bosh)
-IAAS="openstack"                                 # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
-COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"       # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml)
+COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"       # common_vars.yml File Path (e.g. ../../common/common_vars.yml)
 
 # DEPLOY
-bosh -e ${BOSH_NAME} -n -d rabbitmq deploy --no-redact rabbitmq.yml \
+bosh -e ${BOSH_ENVIRONMENT} -n -d rabbitmq deploy --no-redact rabbitmq.yml \
     -l ${COMMON_VARS_PATH} \
     -l vars.yml

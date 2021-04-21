a0bcd5d2dbb14c1bc2ae800fbe009e33c87d8ffd
jinhyojin <hjinjin@crossent.com>
2021-04-21 10:28:31 +0900

    update cce_enable status - mongodb

--- mongodb/deploy.sh
+++ mongodb/deploy.sh
@@ -6,6 +6,7 @@ BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에
 
 # DEPLOY
 bosh -e ${BOSH_ENVIRONMENT} -n -d mongodb deploy --no-redact mongodb.yml \
+	-o operations/cce.yml \
     -l ${COMMON_VARS_PATH} \
     -l vars.yml \
     -l operations/pem.yml
